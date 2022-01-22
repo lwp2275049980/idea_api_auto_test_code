d637821c6d2d4766f46457ac058db17e4c74c18b
lwp2275049980 <17366222065@163.com>
2022-01-22 20:01:37 +0800

    修改玩一玩

--- src/test/java/com/lemon01/testcase/TestGit.java
+++ src/test/java/com/lemon01/testcase/TestGit.java
@@ -3,6 +3,6 @@ package com.lemon01.testcase;
 public class TestGit {
     public static void main(String[] args) {
         System.out.println("git" +
-                "新增我去年一万");
+                "修改再来玩一玩");
     }
 }
