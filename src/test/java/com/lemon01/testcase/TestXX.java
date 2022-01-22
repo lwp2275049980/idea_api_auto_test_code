d7770dc0dda6ebdd030169ef1dbc5ec05b3b8306
lwp2275049980 <17366222065@163.com>
2022-01-22 19:53:00 +0800

    添加一个TestXX的测试类

---
+++ src/test/java/com/lemon01/testcase/TestXX.java
@@ -0,0 +1,11 @@
+package com.lemon01.testcase;
+
+import org.testng.annotations.Test;
+
+public class TestXX {
+    @Test
+    public void testxx(){
+        System.out.println("新增某个文件后提交到远程git服务器");
+
+    }
+}
